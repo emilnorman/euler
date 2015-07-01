e2b956754a03784bb288513d73c7e68568862895
Emil Norman <emimel@marvell.com>
2015-07-01 13:31:53 +0200

    Problem 38 solved

--- problem038.py
+++ problem038.py
@@ -7,26 +7,28 @@
 # i (1, 2, ... , n), n > 1
 # i < 10000
 
+def is_pandigital(num):
+    s = str(num)
+
+    if ('1' in s and '2' in s and '3' in s and '4' in s and '5' in s and
+        '6' in s and '7' in s and '8' in s and '9' in s):
+        return True
+
+    return False
+
 largest = (0, 0, 0)
 
-for i in range(1, 10000):
-#     print("--->" + str(i))
+for i in range(1, 100000):
     factor = 1
     sum = i
     while (sum < 1000000000):
-        if (sum > largest[0]):
-            largest = (sum, i, factor)
+        if (is_pandigital(sum)):
+            if (sum > largest[0]):
+                largest = (sum, i, factor)
 
         factor += 1
         tmp = i * factor
         sum *= 10 ** len(str(tmp))
-#         print("* " + str(tmp) + " - " + str(len(str(tmp))))
         sum += tmp
 
-        print(sum)
-
-#     print(" :" + str(largest))
-
 print(largest)
-
-# 999973182
